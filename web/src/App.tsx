0fb21252a3f1c6563f661c8f643546663ffb0a32
Marcelo <marcelo.arraes@gmail.com>
2024-10-04 21:36:18 +0930

    feat: initializing front end

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div className="bg-zinc-600 flex flex-1 min-h-screen">
+      <h2 className="text-fuchsia-500 text-6xl">Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
